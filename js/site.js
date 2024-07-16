1dc2f2de5689b7c0dc45f8acc8a9e841798dba80
lmatulis <matulisluke@gmail.com>
2024-07-16 16:49:56 -0500

    changed normalization. using regex this time.

--- js/site.js
+++ js/site.js
@@ -3,20 +3,31 @@ function getValues() {
     let inputMessage = document.getElementById("message").value;
     let alertBox = document.getElementById("alertBox");
 
+    //reset alertbox
     alertBox.classList.remove("alert-danger");
     alertBox.classList.remove("alert-success");
     alertBox.style.opacity = 0;
 
+    //get a reversed copy of the message thats been normalized
     let reversedMessage = reverse(inputMessage);
+
+    //check if its a palindrome
     let isPalindrome = checkForPalindrome(inputMessage);
 
+    //display the results
     alertBox.innerHTML = displayResults(isPalindrome, reversedMessage);
 }
 
+//checks if the input message is a palindrome by returning a boolean value
 function checkForPalindrome(inputMessage) {
+    //innocent until proven guilty
     let isPalindrome = true;
-    let normalizedMessage = inputMessage.toLowerCase().replaceAll(" ", "");
+    //setting a range of characters to replace
+    let regex = /\W|_/g
+    //setting a variable that has been normalized
+    let normalizedMessage = inputMessage.replace(regex, "").toLowerCase();
 
+    //loop through the normalized message
     for(let i = 0; i < normalizedMessage.length; i++) {
 
         fromLeft = normalizedMessage[i];
@@ -31,6 +42,7 @@ function checkForPalindrome(inputMessage) {
     return isPalindrome;
 }
 
+//displays results based on if it is a palindrome or not
 function displayResults(isPalindrome, reversedMessage) {
     let message = "";
     let alertElement = document.getElementById("alertBox");
@@ -47,10 +59,12 @@ function displayResults(isPalindrome, reversedMessage) {
     return message;
 }
 
+//normalizes the message and reverses it for output
 function reverse(inputMessage) {
 
-let reversedMessage = "";
-let normalizedMessage = inputMessage.toLowerCase().replaceAll(" ", "");
+    let reversedMessage = "";
+    let regex = /\W|_/g
+    let normalizedMessage = inputMessage.replace(regex, "").toLowerCase();
 
     for(let i = normalizedMessage.length-1; i >= 0; i--) {
         reversedMessage += normalizedMessage[i];
